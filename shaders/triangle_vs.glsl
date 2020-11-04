3d0d6ebfe644b5829243fd40bf973391ff510a30
tzvikif <tzvikif@gmail.com>
2020-11-04 22:44:30 +0200

    read shaders from file

---
+++ shaders/triangle_vs.glsl
@@ -0,0 +1,12 @@
+# version 330 
+
+in vec3 a_position;
+in vec3 a_color;
+
+out vec3 v_color;
+
+void main()
+{
+    gl_Position = vec4(a_position, 1.0);
+    v_color = a_color;
+}
--- triangle.py
+++ triangle.py
@@ -3,33 +3,6 @@ from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
 
-vertex_src = """
-# version 330 
-
-in vec3 a_position;
-in vec3 a_color;
-
-out vec3 v_color;
-
-void main()
-{
-    gl_Position = vec4(a_position, 1.0);
-    v_color = a_color;
-}
-"""
-
-fragment_src = """
-# version 330 
-
-in vec3 v_color;
-out vec4 out_color;
-
-void main()
-{
-    out_color = vec4(v_color, 1.0);
-}
-"""
-
 # initializing glfw library
 if not glfw.init():
     raise Exception("glfw can not be initialized!")
@@ -64,20 +37,20 @@ vertices = [-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
 vertices = np.array(vertices, dtype=np.float32)
 vertex_array_id = glGenVertexArrays(1)
 glBindVertexArray(vertex_array_id)
-shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
+prog = compileProgram(compileShader('shaders/triangle_vs.glsl', GL_VERTEX_SHADER), compileShader('shaders/triangle_fs.glsl', GL_FRAGMENT_SHADER))
 VBO = glGenBuffers(1)
 glBindBuffer(GL_ARRAY_BUFFER, VBO)
 glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
 
-position = glGetAttribLocation(shader, "a_position")
+position = glGetAttribLocation(prog, "a_position")
 glEnableVertexAttribArray(position)
 glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
 
-color = glGetAttribLocation(shader, "a_color")
+color = glGetAttribLocation(prog, "a_color")
 glEnableVertexAttribArray(color)
 glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
 
-glUseProgram(shader)
+glUseProgram(prog)
 glClearColor(0, 0.1, 0.1, 1)
 
 # the main application loop
