7731d69764e6b74ae84b2a051c2931c55159cfd2
tzvikif <tzvikif@gmail.com>
2020-12-03 17:47:11 +0200

    adding shadow

--- shaders/ray_marching_anim_fs.glsl
+++ shaders/ray_marching_anim_fs.glsl
@@ -1,7 +1,8 @@
 float map(in vec3 pos)
 {
     float d = length(pos)-0.25;
-    return d;
+    float d2 = pos.y-(-0.25);
+    return min(d,d2);
     
 }
 vec3 calcNormal(in vec3 pos)
@@ -13,14 +14,8 @@ vec3 calcNormal(in vec3 pos)
 			        map(pos+e.yyx)-map(pos-e.yyx)) );
         
 }
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+float castRay(in vec3 ro ,in vec3 rd)
 {
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 p = (2.0*fragCoord-iResolution.xy)/iResolution.y;
-	vec3 ro = vec3(0.0,0.0,1.0);	//ray origion(camera)
-    vec3 rd = normalize(vec3(p,-1.5));
-    vec3 col = vec3(0.0);
     float t = 0.0;
     for(int i=0;i<100;i++)
     {
@@ -32,15 +27,28 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         if(t>20.0)
             break;
     }
-    if(t<20.0)
+    if(t>20.0)
+        t=-1.0;
+    return t;
+}
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 p = (2.0*fragCoord-iResolution.xy)/iResolution.y;
+	vec3 ro = vec3(0.0,0.0,1.0);	//ray origion(camera)
+    vec3 rd = normalize(vec3(p,-1.5));
+    vec3 col = vec3(0.0);
+    float t = castRay(ro,rd);
+    if(t>0.0)
     {
         vec3 pos = ro+t*rd;
         vec3 nor = calcNormal(pos);
         
         vec3 sun_dir = normalize(vec3(0.8,0.4,-0.2));
         float sun_dif = clamp(dot(sun_dir,nor),0.0,1.0);
+        float sun_sha = step(castRay(pos+nor*0.001,sun_dir),0.0);
         float sky_dir = clamp(0.5+0.5*dot(sun_dir,vec3(0.0,1.0,0.0)),0.0,1.0);
-        col = vec3(1.0,0.7,0.5)*sun_dif;
+        col = vec3(1.0,0.7,0.5)*sun_dif*sun_sha;
         col += vec3(0.0,0.1,0.3)*sky_dir;
     }
     // Output to screen
